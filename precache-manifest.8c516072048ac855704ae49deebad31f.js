c4a7c316a41e28b74d00545c547c43bc4171d732
Code Sandbox <hello@codesandbox.io>
2022-10-20 01:27:11 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8c516072048ac855704ae49deebad31f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a18758e33119adad355e8b9be77341c",
+    "url": "https://ritorads.github.io/csb-jgd54p/index.html"
+  },
+  {
+    "revision": "a9cfc65444c8cd56d27b",
+    "url": "https://ritorads.github.io/csb-jgd54p/static/css/main.93604cf7.chunk.css"
+  },
+  {
+    "revision": "416f10162300033210f0",
+    "url": "https://ritorads.github.io/csb-jgd54p/static/js/2.248e78bf.chunk.js"
+  },
+  {
+    "revision": "a9cfc65444c8cd56d27b",
+    "url": "https://ritorads.github.io/csb-jgd54p/static/js/main.f1ed63e5.chunk.js"
+  },
+  {
+    "revision": "bad5d8b3451c7686a845",
+    "url": "https://ritorads.github.io/csb-jgd54p/static/js/runtime~main.24ca2a4e.js"
+  }
+]);
